c1d16c564004298a54c46f2511a10421d0025c93
Nico Spina <dominicspina23@gmail.com>
2022-11-19 21:12:54 -0500

    website redesign

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
-console.log("test")
+btn = document.getElementById('project')
+  btn.addEventListener('click', () => {
+    open('https://github.com/NicoLoveCode', '_blank');
+  })
+
+btn = document.getElementById('form')
+ btn.addEventListener('click', () => {
+  open('https://nicolovecode.github.io/page2/', '_blank');
+ })
+
+// btn = document.getElementById('other')
+// btn.addEventListener('click', () => {
+  // alert('you clicked the other button')
+// })
+
+btn = document.getElementById('info')
+ btn.addEventListener('click', () => {
+  open('', '_blank');
+ })
+
